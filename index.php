8491f4ca12fc6cebd52c326e58bf99ab5d9692de
mrobles1693 <mar_16_93@hotmail.com>
2018-11-16 18:49:41 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>MI PRIMERA APP PHP</title>
+    </head>
+    <body>
+        <?php
+        $x = 5;
+        $y = 12;
+        
+        $suma = $x + $y;
+        
+        //for ($x; $x++; $x<10){
+          //  echo "<h1>".$suma."</h1></br>";
+        //}
+        
+        ?>
+    </body>
+</html>
---
+++ lenguaje/lenguaje-1.php
@@ -0,0 +1,39 @@
+<?php
+
+$variable = "Hola Mundo";
+
+echo $variable."</br>";
+
+$variable = "Cambio";
+
+print $variable."</br>";
+
+$otra_variable =  "SOY OTRA.";
+
+echo '$otra_variable</br>';
+
+define('tres', 3);
+
+echo tres."</br>";
+
+// String, Integer, Float, Boolean 
+
+$string = "<strong>soy un string</strong></br>";
+
+echo $string;
+
+$integer = 30;
+
+echo $integer."</br>";
+
+$float = 3.14;
+
+echo $float."</br>";
+
+$bolean = true;
+
+if ($bolean == true){    
+    echo "<strong>Es verdad</strong>.</br>";
+}else{
+    echo "<strong>Es falso</strong>.</br>";    
+}
